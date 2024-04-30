9fa5911578f24e1cb8fe8410a20ae6897e0600a1
CrisConSer <cristinacondeserrano@gmail.com>
2024-04-30 21:40:14 +0200

    update

--- DESKTOP/js/script.js
+++ DESKTOP/js/script.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
   // Obtener el contenedor de la zona 4
   const contenedorZona4 = document.querySelector('.contenedor-zona-4');
+  const contenedorZona4bis = document.querySelector('.contenedor-zona-4-bis');
 
   // Agregar evento de clic a todas las imágenes de la zona 3
   imagenZona32.forEach(imagen => {
@@ -84,6 +85,7 @@ document.addEventListener('DOMContentLoaded', function() {
       </div>
     </div>
       `;
+    contenedorZona4bis.style.display = 'none'; 
     });
   });
 
